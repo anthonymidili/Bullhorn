28254804e692b29840dd66942f38b682d362a440
Anthony Midili <tonywinslow@yahoo.com>
2012-12-22 17:28:46 -0500

    Finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      flash[:success] = "You have successfully created your Bookkeeping account!"
       redirect_to @user
     else
       render 'new'
