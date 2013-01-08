70237df813984b54631f47fe5f91e7bb8dbc7957
Anthony Midili <tonywinslow@yahoo.com>
2013-01-07 19:37:46 -0500

    Paginate 5 just a test. Will be removed in next commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
