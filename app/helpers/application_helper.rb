6e608b9e1d54c246662ccdd8309813b9ca5f7332
Anthony Midili <tonywinslow@yahoo.com>
2013-01-22 13:07:57 -0500

    Name changed to @you

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns the full title on a per-paage basis.
   def full_title(page_title)
-    base_title = "@YOU"
+    base_title = "@you"
     if page_title.empty?
       base_title
     else
