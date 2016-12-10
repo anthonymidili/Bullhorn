ab21fe61fb508328ad270aacedee4a24bf4094a6
Anthony Midili <tonywinslow@yahoo.com>
2016-12-10 17:49:14 -0500

    Run rails rails:update to update rails from 4 to 5. Create a procfile and use puma as the default webrick. Use figaro gem for secrts.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,14 @@
 source 'https://rubygems.org'
 
 gem 'rails', '~> 5.0.0'
+
+# puma inplace of Default Webrick
+gem 'puma', '~> 3.6.0'
+# rack timeout needed for puma
+gem 'rack-timeout', '~> 0.4.2'
+
 gem 'bootstrap-sass', '~> 3.3.5'
+# To use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.5'
 gem 'will_paginate', '~> 3.1.0'
 gem 'bootstrap-will_paginate', '~> 0.0.10'
@@ -12,6 +19,9 @@ gem 'bootstrap-will_paginate', '~> 0.0.10'
 group :development, :test do
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'rspec-rails', '~> 3.5.0'
+  gem 'capybara', '~> 2.10.1'
+
 end
 
 gem 'sass-rails',   '~> 5.0.6'
@@ -24,8 +34,8 @@ gem 'pg', '~> 0.19.0'
 
 gem 'rails_12factor', '~> 0.0.3', group: :production
 
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
+# https://github.com/laserlemon/figaro
+gem 'figaro', '~> 1.1.1'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
--- config/routes.rb
+++ config/routes.rb
@@ -17,47 +17,4 @@ Rails.application.routes.draw do
   get :help, to: 'static_pages#help'
   get :about, to: 'static_pages#about'
   get :contact, to: 'static_pages#contact'
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
