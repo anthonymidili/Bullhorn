a307cf9f757148afb3662074b2c2db86333abe2c
Anthony Midili <tonywinslow@yahoo.com>
2016-12-13 21:47:18 -0500

    Minor view changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,7 @@ class ApplicationController < ActionController::Base
   def signed_in_user
     unless signed_in?
       store_location
-      flash[:warning] = 'Please sign in.'
-      redirect_to signin_path
+      redirect_to signin_path, notice: 'Please sign in.'
     end
   end
 
