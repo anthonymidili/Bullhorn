175a61e241ac51e52dcb9a8188ff3fad12c3a7f3
Anthony Midili <tonywinslow@yahoo.com>
2016-12-17 13:54:21 -0500

    Change @you string references to Bullhorn.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns the full title on a per-paage basis.
   def full_title(page_title)
-    base_title = '@you'
+    base_title = 'Bullhorn'
     if page_title.empty?
       base_title
     else
