9b0f9809b6becb4dee2c985fe67919e0d3d91099
Anthony Midili <tonywinslow@yahoo.com>
2016-12-17 10:37:54 -0500

    Add follow/unfollow button on users index.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,8 @@ module UsersHelper
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+
+  def button_size(param_action)
+    param_action == 'index' ? 'small' : 'large'
+  end
 end
