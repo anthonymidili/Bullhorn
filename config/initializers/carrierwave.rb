4eb7606c5da90dc8e68afdcdf80097d055c75f0f
Anthony Midili <tonywinslow@yahoo.com>
2016-12-21 00:58:18 -0500

    Try new carrierwave initializer.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,28 @@
-# Use local storage if in development or test
-if Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
-  end
-end
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_credentials = {
+        # Configuration for Amazon S3 should be made available through an Environment variable.
+        # For local installations, export the env variable through the shell OR
+        # if using Passenger, set an Apache environment variable.
+        #
+        # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+        #
+        # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
 
-# Use AWS storage if in production
-if Rails.env.production?
-  CarrierWave.configure do |config|
+        # Configuration for Amazon S3
+        provider: 'AWS',
+        aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', ''),
+        aws_secret_access_key: ENV.fetch('AWS_SECRET_KEY', '')
+    }
     config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+
+    # To let CarrierWave work on heroku
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+  else
+    # For testing, upload files to local `tmp` folder.
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
   end
-
-  config.fog_credentials = {
-      :provider               => 'AWS',                             # required
-      :aws_access_key_id      => ENV.fetch('AWS_ACCESS_KEY_ID', ''),            # required
-      :aws_secret_access_key  => ENV.fetch('AWS_SECRET_KEY', '')# ,     # required
-      # :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']               # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-
 end
