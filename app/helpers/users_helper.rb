74e56b9f537ea1be05b358c19addda8a12f2a34d
Anthony Midili <tonywinslow@yahoo.com>
2016-12-21 21:39:38 -0500

    Refactor users helper method avatar_for(user, size).

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,16 @@
 module UsersHelper
 
   def avatar_for(user, size)
-    if user.avatar
-      image_tag(user.avatar.image.thumb, size: size, class: 'avatar')
-    else
-      image_tag('default_avatar.png', size: size, class: 'avatar')
-    end
+    image_tag(select_avatar_for(user), size: size, alt: user.name, class: 'avatar')
   end
 
   def button_size(param_action)
     param_action == 'show' ? 'large' : 'small'
+  end
+
+private
+
+  def select_avatar_for(user)
+    user.avatar ? user.avatar.image.thumb : 'default_avatar.png'
   end
 end
