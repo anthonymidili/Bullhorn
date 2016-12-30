9fcbb19ca09d03c5465095e15ba9e5686fc258b3
Anthony Midili <tonywinslow@yahoo.com>
2016-12-30 05:29:12 -0500

    Validate content presence.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :created_by_user, foreign_key: 'created_by_user_id', class_name: 'User'
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :destroy
+
+  validates :content, presence: true
 end
