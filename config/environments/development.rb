4d8ef65019d05cafb4ef0765b453f9a140f3dfab
Anthony Midili <tonywinslow@yahoo.com>
2017-01-11 19:43:40 -0500

    Prevent rendering comments/comments if not on the micropost show page.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['DEFAULT_URL'] }
 
   config.after_initialize do
-    Bullet.enable = false
-    Bullet.alert = false
+    Bullet.enable = true
+    Bullet.alert = true
 
     # Bullet.bullet_logger = true
     # Bullet.console = true
