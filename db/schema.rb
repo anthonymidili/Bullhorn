93d453633fcf27ecc0bec1bdb8739cc222c69da9
AMidili <tonywinslow@yahoo.com>
2017-11-18 21:58:26 -0500

    Change microposts content from string to text db format.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118134923) do
+ActiveRecord::Schema.define(version: 20171119024757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20171118134923) do
   end
 
   create_table "microposts", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
