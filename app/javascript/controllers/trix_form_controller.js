1b7d91dfe1da86d7b7b674bea4459cd23cc45f97
Anthony Midili <tonywinslow@yahoo.com>
2023-11-15 06:59:36 -0500

    Stimulus-fi allowed file types for trix editor.

--- app/javascript/controllers/trix_form_controller.js
+++ app/javascript/controllers/trix_form_controller.js
@@ -4,7 +4,20 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['submit']
 
-  checkTypeDisableSubmit(/*event*/) {
+  checkType(event) {
+    const allowedTypes = ["image/png", "image/jpg", "image/jpeg", "image/gif", 
+    "image/tiff", "image/bmp", "video/mp4", "video/mpeg", "video/mpg", "video/mov", 
+    "video/avi", "video/wmv", "video/m4v", "video/flv", "video/webm", "video/mkv", 
+    "video/m2v", "audio/mp3", "audio/ogg", "audio/m4a", "audio/wma", "audio/wav", 
+    "audio/aac", "audio/flac", "audio/aiff", "audio/aif", "audio/mpeg"]
+
+    if (!allowedTypes.includes(event.file.type)) {
+      event.preventDefault()
+      alert("This filetype is not supported.")
+    }
+  }
+
+  disableSubmit(/*event*/) {
     const { hasTrixAttachmentsUploading } = this
     this.submitTargets.forEach(submitTarget => submitTarget.disabled = hasTrixAttachmentsUploading)
   }
@@ -19,24 +32,5 @@ export default class extends Controller {
 
   get trixElements() {
     return Array.from(this.element.querySelectorAll("trix-editor"))
-  }
-
-  connect() {
-    const allowedTypes = ["image/png", "image/jpg", "image/jpeg", "image/gif", 
-    "image/tiff", "image/bmp", "video/mp4", "video/mpeg", "video/mpg", "video/mov", 
-    "video/avi", "video/wmv", "video/m4v", "video/flv", "video/webm", "video/mkv", 
-    "video/m2v", "audio/mp3", "audio/ogg", "audio/m4a", "audio/wma", "audio/wav", 
-    "audio/aac", "audio/flac", "audio/aiff", "audio/aif", "audio/mpeg"]
-
-    function fileNotExcepted(e) {
-      e.preventDefault()
-      alert("This filetype is not supported.")
-    }
-
-    document.addEventListener("trix-file-accept", e => {
-      if (!allowedTypes.includes(e.file.type)) {
-        fileNotExcepted(e)
-      }
-    })
   }
 }
