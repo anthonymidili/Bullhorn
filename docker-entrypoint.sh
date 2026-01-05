bb0028aa5963fc7ccc2add4e8f6ebf78f6af80e6
Anthony Midili <tonywinslow@yahoo.com>
2026-01-05 01:42:50 -0500

    Remove log level 3.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 bundle exec rails db:migrate
 
 echo "Starting Passenger on port ${PORT:-3000}..."
-exec bundle exec passenger start -e production --port "${PORT:-3000}" --address 0.0.0.0 --log-level 3
+exec bundle exec passenger start -e production --port "${PORT:-3000}" --address 0.0.0.0
