85c1f042038b0551705cc8833f67866bfdd85c00
Anthony Midili <tonywinslow@yahoo.com>
2026-08-14 08:14:31 -0400

    Update cache version to v3 in service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for BullhornXL PWA
-const CACHE_NAME = 'bullhornxl-v2';
+const CACHE_NAME = 'bullhornxl-v3';
 const urlsToCache = [
   '/',  // Home page
   '/manifest.json'  // Web app manifest
