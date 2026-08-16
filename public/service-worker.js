ecb1fb1dd408bb3e2928d4c83aca61e645895010
Anthony Midili <tonywinslow@yahoo.com>
2026-08-16 18:56:34 -0400

    Update app icons and increment cache version to v4 in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for BullhornXL PWA
-const CACHE_NAME = 'bullhornxl-v3';
+const CACHE_NAME = 'bullhornxl-v4';
 const urlsToCache = [
   '/',  // Home page
   '/manifest.json'  // Web app manifest
